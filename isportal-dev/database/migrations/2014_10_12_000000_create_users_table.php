e6561b7b8ea8482fa08b11063cc8d76dce8027a2
OliveiraRuben <rubenoliveira94@gmail.com>
2017-05-25 22:46:00 +0100

    Add column to user table

--- isportal-dev/database/migrations/2014_10_12_000000_create_users_table.php
+++ isportal-dev/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('report');
             $table->string('password', 60);
             $table->enum('type', array('student', 'organization' , 'professor'));
             $table->rememberToken();
