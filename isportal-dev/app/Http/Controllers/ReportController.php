8a7fb724531fccefbdf26acc3358f8d525924bcb
lmaltez5 <lmaltez5@gmail.com>
2017-05-29 16:31:19 +0100

    Update ReportController.php

--- isportal-dev/app/Http/Controllers/ReportController.php
+++ isportal-dev/app/Http/Controllers/ReportController.php
@@ -11,6 +11,9 @@ use App\Http\Requests;
 use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use ZipArchive;
+use \RecursiveIteratorIterator;
+use \RecursiveDirectoryIterator;
 
 class ReportController extends Controller
 {
@@ -47,6 +50,56 @@ class ReportController extends Controller
 
       return response()->download($file, $report, $headers);
     }
+
+
+    public function downloadReportsZipa(){
+        $dir = __DIR__."/../../../reports/";
+        $zip_file = 'file.zip';
+
+        // Get real path for our folder
+        $rootPath = realpath($dir);
+
+        // Initialize archive object
+        $zip = new ZipArchive();
+        $zip->open($zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+        // Create recursive directory iterator
+        /** @var SplFileInfo[] $files */
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($rootPath),
+            RecursiveIteratorIterator::LEAVES_ONLY
+        );
+
+        foreach ($files as $name => $file)
+        {
+            // Skip directories (they would be added automatically)
+            if (!$file->isDir())
+            {
+                // Get real and relative path for current file
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+                // Add current file to archive
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+
+        // Zip archive will be created only after closing object
+        $zip->close();
+
+
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename='.basename($zip_file));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($zip_file));
+        readfile($zip_file);
+
+    }
+
 
     public function index(){
         return view('report.index');
